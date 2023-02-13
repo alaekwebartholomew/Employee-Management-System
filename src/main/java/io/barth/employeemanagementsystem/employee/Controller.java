bbb9de7d862756a11998899aceb2694a0bd9e96d
alaekwebartholomew <alaekwebartholomew@gmail.com>
2023-02-13 06:37:37 -0800

    Entity

--- src/main/java/io/barth/employeemanagementsystem/employee/Controller.java
+++ src/main/java/io/barth/employeemanagementsystem/employee/Controller.java
@@ -1,4 +1,13 @@
 package io.barth.employeemanagementsystem.employee;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
+
+    @GetMapping("employee")
+    public String sayHello(){
+        return "Hello!";
+    }
 }
--- src/main/java/io/barth/employeemanagementsystem/employee/Employee.java
+++ src/main/java/io/barth/employeemanagementsystem/employee/Employee.java
@@ -1,4 +1,21 @@
 package io.barth.employeemanagementsystem.employee;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class Employee {
+
+    @Id
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private Long age;
+    private String location;
+    private String email;
+    private Boolean remote;
+    private String department;
 }
