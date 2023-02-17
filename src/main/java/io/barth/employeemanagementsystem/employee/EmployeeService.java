8583a6538bbb4911b053665e00c648a9f1dc5b44
alaekwebartholomew <alaekwebartholomew@gmail.com>
2023-02-17 07:47:07 +0100

    query delete rep

--- src/main/java/io/barth/employeemanagementsystem/employee/EmployeeService.java
+++ src/main/java/io/barth/employeemanagementsystem/employee/EmployeeService.java
@@ -25,4 +25,6 @@ public interface EmployeeService {
 
     public List<Employee> getEmployeeByFirstNameOrLocation(String name, String location);
 
+    Integer deleteEmployeeByFirstName(String name);
+
 }
