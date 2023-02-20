b7471f368727a0102b47d48563ace00d2d95e6f6
alaekwebartholomew <alaekwebartholomew@gmail.com>
2023-02-21 00:43:02 +0100

    department entity

---
+++ src/main/java/io/barth/employeemanagementsystem/department/DepartmentRepository.java
@@ -0,0 +1,8 @@
+package io.barth.employeemanagementsystem.department;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
